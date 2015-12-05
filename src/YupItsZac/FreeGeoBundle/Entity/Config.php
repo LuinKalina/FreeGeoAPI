2f4a7bc7a31d32e3cadfb69144b7054639be7886
= <me@yupitszac.com>
2015-12-05 15:25:17 +0100

    Added Config:: entity, moved some stuff to it

---
+++ src/YupItsZac/FreeGeoBundle/Entity/Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yupitszac
+ * Date: 05.12.15
+ * Time: 15:18
+ */
+
+namespace YupItsZac\FreeGeoBundle\Entity;
+
+
+class Config {
+
+    const MAILGUN_API_KEY = '..';
+    const MAILGUN_API_URL = '..';
+
+    const FROM_EMAIL_ADDRESS = 'noreply@freegeoapi.org';
+    const ENVIRONMENT = 'dev';
+
+}
