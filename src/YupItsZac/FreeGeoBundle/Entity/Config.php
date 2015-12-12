a43e7a81438d765f0b69c864170e1f02ae3ad461
= <me@yupitszac.com>
2015-12-12 16:20:30 +0100

    Contib fix for prod issue

--- src/YupItsZac/FreeGeoBundle/Entity/Config.php
+++ src/YupItsZac/FreeGeoBundle/Entity/Config.php
@@ -16,14 +16,16 @@ class Config {
     const MAILGUN_API_KEY = '...';
     const MAILGUN_API_URL = '...';
 
-    const FROM_EMAIL_ADDRESS = 'freegeo@yupitszac.com';
+    const FROM_EMAIL_ADDRESS = 'noreply@freegeoapi.org';
     const ENVIRONMENT = 'dev';
 
-    const PUBLIC_ADDRESS = 'www.freegeoapi.org';
+    const PUBLIC_ADDRESS = 'freegeoapi.org';
     const WEB_PROTOCOL_PROD = 'https://';
     const WEB_PROTOCOL_DEV = 'http://';
 
-    const BASE_URL_PROD = 'https://www.freegeoapi.org';
+    const WEB_ADDRESS_LOCAL = 'http://freegeo.dev';
+
+    const BASE_URL_PROD = 'https://freegeoapi.org';
 
     const GITHUB_MAIN_REPO = 'http://www.github.com/YupItsZac/FreeGeoAPI';
 
