e944040020c1339392793e00aa387a09c76e8d43
= <me@yupitszac.com>
2015-12-27 01:00:40 +0100

    Added variable values for deployment script

--- src/YupItsZac/FreeGeoBundle/Entity/Config.php
+++ src/YupItsZac/FreeGeoBundle/Entity/Config.php
@@ -13,8 +13,8 @@ class Config {
 
     const PROJECT_NAME = 'FreeGeo API';
 
-    const MAILGUN_API_KEY = 'key-78cb2ed8a825cd58ad869a766a16389f';
-    const MAILGUN_API_URL = 'https://api.mailgun.net/v3/mailer.freegeoapi.org';
+    const MAILGUN_API_KEY = '{MAILGUN_API_KEY}';
+    const MAILGUN_API_URL = '{MAILGUN_API_URL}';
     const API_STATUS_CHECK_SESSION_TOKEN = 'a685fb3bd11540e7dcbd75327e273170';
     const API_STATUS_CHECK_PUBLIC_KEY = '3ce56e8a4fbada15e5d68bb0dcabb3de';
 
