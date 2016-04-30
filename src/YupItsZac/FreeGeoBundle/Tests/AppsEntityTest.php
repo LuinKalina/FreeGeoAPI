5cf5f2c451f6d9da0a7c110ac559defb0ac6ad00
Zac Brown <me@yupitszac.com>
2016-04-30 20:36:13 +0200

    Added more test

--- src/YupItsZac/FreeGeoBundle/Tests/AppsEntityTest.php
+++ src/YupItsZac/FreeGeoBundle/Tests/AppsEntityTest.php
@@ -1,7 +1,60 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: payleven
- * Date: 30.04.16
- * Time: 19:47
- */
+
+namespace YupItsZac\FreeGeoBundle\Tests;
+
+use YupItsZac\FreeGeoBundle\Entity\Apps;
+
+class AppsEntityTest extends \PHPUnit_Framework_TestCase {
+
+    private $app;
+
+    public function setUp() {
+
+        $this->app = new Apps();
+        $this->app->setAssoc(1);
+        $this->app->setAppdescription('testdescription');
+        $this->app->setApptitle('testtitle');
+        $this->app->setAppwebsite('testwebsite');
+        $this->app->setBeta(1);
+        $this->app->setHash('testhash');
+        $this->app->setPublickey('testpublickey');
+        $this->app->setSecretkey('testsecretkey');
+        $this->app->setStatus('teststatus');
+    }
+
+    public function testGetAssoc() {
+        $this->assertEquals(1, $this->app->getAssoc());
+    }
+
+    public function testGetAppDescription() {
+        $this->assertEquals('testdescription', $this->app->getAppdescription());
+    }
+
+    public function testGetApptitle() {
+        $this->assertEquals('testtitle', $this->app->getApptitle());
+    }
+
+    public function testGetAppwebsite() {
+        $this->assertEquals('testwebsite', $this->app->getAppwebsite());
+    }
+
+    public function testGetBeta() {
+        $this->assertEquals(1, $this->app->getBeta());
+    }
+
+    public function testGetHash() {
+        $this->assertEquals('testhash', $this->app->getHash());
+    }
+
+    public function testGetPublickey() {
+        $this->assertEquals('testpublickey', $this->app->getPublickey());
+    }
+
+    public function testGetSecretKey() {
+        $this->assertEquals('testsecretkey', $this->app->getSecretkey());
+    }
+
+    public function testGetStatus() {
+        $this->assertEquals('teststatus', $this->app->getStatus());
+    }
+}
--- src/YupItsZac/FreeGeoBundle/Tests/WebControllerTest.php
+++ src/YupItsZac/FreeGeoBundle/Tests/WebControllerTest.php
@@ -1,7 +1,83 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: payleven
- * Date: 30.04.16
- * Time: 20:01
- */
+
+namespace YupItsZac\FreeGeoBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class WebControllerTest extends WebTestCase {
+
+    private $client;
+
+    public function setUp() {
+        $this->client = static::createClient();
+    }
+
+    public function testIndexPage() {
+
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Easy. Reliable. Free. Open Source.")')->count());
+    }
+
+    public function testStatusPage() {
+        $crawler = $this->client->request('GET', '/status');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("API Status Report")')->count());
+    }
+
+    public function testRegisterPage() {
+
+        $crawler = $this->client->request('GET', '/status');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("API Status Report")')->count());
+    }
+
+    public function testDocsHomePage() {
+
+        $crawler = $this->client->request('GET', '/docs');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Documentation")')->count());
+    }
+
+    public function testDocsAuthenticate() {
+
+        $crawler = $this->client->request('GET', '/docs/authenticate');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Session Authentication")')->count());
+    }
+
+    public function testDocsDistanceCalculation() {
+
+        $crawler = $this->client->request('GET', '/docs/requests/calculate/distance');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Distance Calculation")')->count());
+    }
+
+    public function testDocsDetectTimezone() {
+
+        $crawler = $this->client->request('GET', '/docs/requests/detect/timezone');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Timezone Detection")')->count());
+    }
+
+    public function testDocsFindNearCities() {
+
+        $crawler = $this->client->request('GET', '/docs/requests/find-near/cities');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Nearest Cities")')->count());
+    }
+
+    public function testDocsFindNearPorts() {
+
+        $crawler = $this->client->request('GET', '/docs/requests/find-near/ports');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Nearest Ports")')->count());
+    }
+
+    public function testDocsFindNearAirports() {
+
+        $crawler = $this->client->request('GET', '/docs/requests/find-near/airports');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Nearest Airports")')->count());
+    }
+}
