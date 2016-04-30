3ebd981932ac1d3c02d1e0e0bf619b08687c90d1
Zac Brown <me@yupitszac.com>
2016-04-30 19:31:39 +0200

    Tets

--- src/YupItsZac/FreeGeoBundle/Tests/AirportsEntityTest.php
+++ src/YupItsZac/FreeGeoBundle/Tests/AirportsEntityTest.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: payleven
- * Date: 30.04.16
- * Time: 11:19
- */
+
+namespace YupItsZac\FreeGeoBundle\Tests;
+
+use YupItsZac\FreeGeoBundle\Controller\DeveloperController;
+use YupItsZac\FreeGeoBundle\Entity\Airports;
+
+class AirportsEntityTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $airportsEntity;
+
+    public function setUp()
+    {
+        $this->airportsEntity = new Airports();
+        $this->airportsEntity->setCoordinatesWkt('testcoordinates')
+            ->setType('testtype')
+            ->setIataCode('testcode')
+            ->setName('testairport');
+
+    }
+
+    public function testGetCoordinatesWkt() {
+        $this->assertEquals('testcoordinates', $this->airportsEntity->getCoordinatesWkt());
+    }
+
+    public function testGetType() {
+        $this->assertEquals('testtype', $this->airportsEntity->getType());
+    }
+
+    public function testGetName() {
+        $this->assertEquals('testairport', $this->airportsEntity->getName());
+    }
+
+    public function testGetIataCode() {
+        $this->assertEquals('testcode', $this->airportsEntity->getIataCode());
+    }
+
+
+}
--- src/YupItsZac/FreeGeoBundle/Tests/ApiRequestObjectTest.php
+++ src/YupItsZac/FreeGeoBundle/Tests/ApiRequestObjectTest.php
@@ -1,7 +1,49 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: payleven
- * Date: 30.04.16
- * Time: 14:36
- */
+
+namespace YupItsZac\FreeGeoBundle\Tests;
+
+use YupItsZac\FreeGeoBundle\Entity\ApiRequestObject;
+
+class ApiRequestObjectTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $apiRequestObject;
+
+    public function setUp()
+    {
+        $this->apiRequestObject = new ApiRequestObject();
+
+        $this->apiRequestObject->setLatitude('testlatitude');
+        $this->apiRequestObject->setLongitude('testlongitude');
+        $this->apiRequestObject->setLimit('testlimit');
+        $this->apiRequestObject->setMaximum('testmaximum');
+        $this->apiRequestObject->setMetricUnit('testmetric');
+
+    }
+
+    public function testInstanceOfObject() {
+        $this->assertInstanceOf('YupItsZac\FreeGeoBundle\Entity\ApiRequestObject', $this->apiRequestObject);
+    }
+
+    public function testGetLongitude() {
+        $this->assertEquals('testlongitude', $this->apiRequestObject->getLongitude());
+    }
+
+    public function testGetLatitude() {
+        $this->assertEquals('testlatitude', $this->apiRequestObject->getLatitude());
+    }
+
+    public function testGetLimit() {
+        $this->assertEquals('testlimit', $this->apiRequestObject->getLimit());
+    }
+
+    public function testGetMaximum() {
+        $this->assertEquals('testmaximum', $this->apiRequestObject->getMaximum());
+    }
+
+    public function testGetMetricUnit() {
+        $this->assertEquals('testmetric', $this->apiRequestObject->getMetricUnit());
+    }
+
+
+}
